0ba25da26b9f493e781afb525a7e596ffebe33d7
bobrik717 <bobrik717@rambler.ru>
2016-06-21 15:43:53 +0300

    finish start simfony

--- src/AppBundle/Controller/HomeController.php
+++ src/AppBundle/Controller/HomeController.php
@@ -2,8 +2,10 @@
 
 namespace AppBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
 class HomeController extends Controller
@@ -16,19 +18,43 @@ class HomeController extends Controller
      */
     public function testAction($name)
     {
-        $texts = [
-            'adsf sad fasdf asdf asdf asdf asd',
-            'asdf safd safdsfdaasfd 34434  sdf',
-            'sdaf323 234 23 we qwer q32 32 23 s'
-        ];
         return $this->render('home/test.html.twig',[
             'name' => $name,
-            'texts' => $texts
         ]);
     }
 
+    /**
+     * @Route("/test/{name}/text",name="site_show_text")
+     * @Method("GET")
+     *
+     * @param $name
+     * @return JsonResponse
+     */
+    public function getTextAction($name)
+    {
+        $texts = [
+            ['id' => 1, 'username' => 'AquaPelham', 'avatarUri' => '/images/leanna.jpeg', 'note' => 'Octopus asked me a riddle, outsmarted me', 'date' => 'Dec. 10, 2015'],
+            ['id' => 2, 'username' => 'AquaWeaver', 'avatarUri' => '/images/ryan.jpeg', 'note' => 'I counted 8 legs... as they wrapped around me', 'date' => 'Dec. 1, 2015'],
+            ['id' => 3, 'username' => 'AquaPelham', 'avatarUri' => '/images/leanna.jpeg', 'note' => 'Inked!', 'date' => 'Aug. 20, 2015'],
+        ];
+
+        $data = [
+            'texts' => $texts
+        ];
+
+        return new JsonResponse($data);
+    }
+
+    /**
+     * @Route("/")
+     *
+     * @param $name
+     * @return Response
+     */
     public function indexAction($name)
     {
-        return $this->render('', array('name' => $name));
+        return $this->render('home/index.html.twig', [
+            'name' => $name]
+        );
     }
 }
