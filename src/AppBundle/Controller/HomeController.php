285ebb8fdf871d1f74f795591ee8848365bd911b
bobrik717 <bobrik717@rambler.ru>
2016-06-27 17:03:19 +0300

    peace

--- src/AppBundle/Controller/HomeController.php
+++ src/AppBundle/Controller/HomeController.php
@@ -18,8 +18,21 @@ class HomeController extends Controller
      */
     public function testAction($name)
     {
+        $funFact = 'Octopuses can change the color of their body in just *three-tenths* of a second!';
+
+        $cache = $this->get('doctrine_cache.providers.my_markdown_cache');
+
+        $key = md5($funFact);
+        if($cache->contains($key)){
+            $funFact = $cache->fetch($key);
+        }else{
+            $funFact = $this->get('markdown.parser')->transformMarkdown($funFact);
+            $cache->save($key,$funFact);
+        }
+
         return $this->render('home/test.html.twig',[
             'name' => $name,
+            'fact' => $funFact,
         ]);
     }
 
@@ -39,22 +52,20 @@ class HomeController extends Controller
         ];
 
         $data = [
-            'texts' => $texts
+            'texts' => $texts,
         ];
 
         return new JsonResponse($data);
     }
 
     /**
-     * @Route("/")
-     *
-     * @param $name
      * @return Response
      */
-    public function indexAction($name)
+    public function indexAction()
     {
         return $this->render('home/index.html.twig', [
-            'name' => $name]
+            //'name' => $name
+            ]
         );
     }
 }
